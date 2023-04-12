d7f948b9138deaf827578e39ee745ae3d14fc1ef
devstefancho <skku7714@gmail.com>
2023-04-13 00:33:49 +0900

    Package: install prettier, husky, lint-staged

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
-import * as React from "react";
-import type { HeadFC, PageProps } from "gatsby";
+import * as React from 'react'
+import type { HeadFC, PageProps } from 'gatsby'
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <main>
       <h1>Home Page</h1>
     </main>
-  );
-};
+  )
+}
 
-export default IndexPage;
+export default IndexPage
 
-export const Head: HeadFC = () => <title>Home Page</title>;
+export const Head: HeadFC = () => <title>Home Page</title>
