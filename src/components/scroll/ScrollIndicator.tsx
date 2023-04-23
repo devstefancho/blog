b8a2dce52d84714a087230c62b458afe7eed2b79
devstefancho <skku7714@gmail.com>
2023-04-23 22:43:11 +0900

    💄 Add Scroll Indicator

---
+++ src/components/scroll/ScrollIndicator.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react'
+
+const ScrollIndicator = () => {
+  const [translateX, setTranslateX] = useState(0)
+  useEffect(() => {
+    const onScroll = () => {
+      const winScroll = window.scrollY
+      const height =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight
+      const move = (winScroll / height) * 100
+
+      setTranslateX(move)
+    }
+
+    window.addEventListener('scroll', onScroll)
+
+    return () => {
+      window.removeEventListener('scroll', onScroll)
+    }
+  }, [])
+
+  return (
+    <div
+      style={{
+        position: 'fixed',
+        top: 0,
+        left: '-100%',
+        width: '100%',
+        height: 3,
+        background: 'var(--contrast)',
+        zIndex: 99,
+        transform: `translateX(${translateX}%)`,
+        transition: 'transform 0.2s ease',
+      }}
+    ></div>
+  )
+}
+
+export { ScrollIndicator }
--- src/pages/blog/{markdownRemark.frontmatter__slug}.tsx
+++ src/pages/blog/{markdownRemark.frontmatter__slug}.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { PageProps, graphql } from 'gatsby'
 import { Layout } from '@/components/layout'
+import { ScrollIndicator } from '@/components/scroll/ScrollIndicator'
 
 export default function BlogPostTemplate({
   data,
 }: PageProps<Queries.markdownTestQuery>) {
   const { markdownRemark } = data
+
   return (
     <Layout>
+      <ScrollIndicator />
       <article>
         <h1>{markdownRemark?.frontmatter?.title}</h1>
         <time dateTime={markdownRemark?.frontmatter?.date ?? ''}>
